ab2b99ef973c12b224e274f40f7856e8a2f6724b
rabol <steen@rabol.com>
2021-08-14 06:50:53 +0200

    Update

--- src/LaravelSimplesubscriptionStripe.php
+++ src/LaravelSimplesubscriptionStripe.php
@@ -2,6 +2,11 @@
 
 namespace Rabol\LaravelSimplesubscriptionStripe;
 
+use App\Models\User;
+use Stripe\StripeClient;
+use Stripe\Exception\ApiErrorException;
+use Log;
+
 class LaravelSimplesubscriptionStripe
 {
 }
