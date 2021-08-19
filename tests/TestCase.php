6dd182b7cec3a673e83360e3d19d1d79809617ec
rabol <steen@rabol.com>
2021-08-19 12:34:44 +0200

    Namespace cleanup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,7 +4,7 @@ namespace Rabol\LaravelSimpleSubscriptionStripe\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
-use Rabol\LaravelSimpleSubscriptionStripe\LaravelSimpleSubScriptionStripeServiceProvider;
+use Rabol\LaravelSimpleSubscriptionStripe\LaravelSimpleSubscriptionStripeServiceProvider;
 
 class TestCase extends Orchestra
 {
@@ -20,7 +20,7 @@ class TestCase extends Orchestra
     protected function getPackageProviders($app)
     {
         return [
-            LaravelSimpleSubScriptionStripeServiceProvider::class,
+            LaravelSimpleSubscriptionStripeServiceProvider::class,
         ];
     }
 
