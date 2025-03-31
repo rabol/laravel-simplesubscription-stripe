0c0dccfceb44b638e6f303457cf222a1ea74f761
rabol <steen@rabol.com>
2025-03-31 08:47:19 +0200

    Update to latest version of dependencies an stripe webhook url in config

--- config/simplesubscription-stripe.php
+++ config/simplesubscription-stripe.php
@@ -5,5 +5,6 @@ return [
     'stripe_key' => env('STRIPE_KEY'),
     'stripe_secret' => env('STRIPE_SECRET'),
     'stripe_webhook_secret' => env('STRIPE_WEBHOOK_SECRET'), // Web hook secret
-    'stripe_webhook_tolerance' => env('STRIPE_WEBHOOK_TOLERANCE', 300) // max time diff in webhook signature
+    'stripe_webhook_tolerance' => env('STRIPE_WEBHOOK_TOLERANCE', 300), // max time diff in webhook signature
+    'stripe_webhook_url' => env('STRIPE_WEBHOOK_URL', env('APP_URL') . '/stripe/webhook') // webhook url
 ];
